8deab0ad1dd7e48cc42c40f393efed78fa20e8b3
Nicu Chiriac <nicu.chiriac99@yahoo.com>
2022-12-01 02:00:43 +0200

    Auth endpoints: login, register added. Auth with JWT added. More CRUD endpoints for products added.

--- Backend/flaskrestaurantapp/__init__.py
+++ Backend/flaskrestaurantapp/__init__.py
@@ -1,15 +1,23 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import os
+from flask_jwt_extended import JWTManager
+from datetime import timedelta
+
 
 host = os.getenv('host')
 user = os.getenv('user')
 password = os.getenv('password')
 database = os.getenv('database')
+secret = os.getenv('JWT_SECRET_KEY')
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://'+ str(user) + ':' + str(password) +'@' + str(host) +'/' + str(database)
 db = SQLAlchemy(app)
-
+app.config["JWT_SECRET_KEY"] = str(secret)
+app.config["JWT_TOKEN_LOCATION"] = ["headers", "cookies", "json", "query_string"]
+app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=1)
+app.config["JWT_REFRESH_TOKEN_EXPIRES"] = timedelta(days=14)
+jwt = JWTManager(app)
 
 from flaskrestaurantapp import routes
--- Backend/flaskrestaurantapp/routes.py
+++ Backend/flaskrestaurantapp/routes.py
@@ -6,28 +6,80 @@ from flask import jsonify, request
 import json
 import functools
 from flaskrestaurantapp import db
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import jwt_required
+from flask_jwt_extended import set_access_cookies
+from flask_jwt_extended import unset_jwt_cookies
+from flask_jwt_extended import create_refresh_token
+from flask_jwt_extended import get_jwt_identity
+from flask_bcrypt import Bcrypt
 
 
-#Un model de validator
-def validate_request(f):
-  @functools.wraps(f)
-  def decorated_function(*args, **kws):
-    data = flask.request.get_json()
+bcrypt = Bcrypt(app)
+
+# Authentication
+# =============================== 
+@app.route('/register', methods=['POST'])
+def register():
+    data = request.get_json()
     if not data:
-      flask.abort(404)
-    return f(*args, **kws)
-  return decorated_function
+        return jsonify({"msg": "Missing JSON in request"}), 400
+    username = data['username']
+    email = data['email']
+    hashed_password = bcrypt.generate_password_hash(data['password']).decode('utf-8')
+    if not username:
+        return jsonify({"msg": "Missing username parameter"}), 400
+    if not email:
+        return jsonify({"msg": "Missing email parameter"}), 400
+    if not hashed_password:
+        return jsonify({"msg": "Missing password parameter"}), 400
+    user = User.query.filter_by(username=username).first()
+    if user:
+        return jsonify({"msg": "Username already exists"}), 400
+    user = User.query.filter_by(email=email).first()
+    if user:
+        return jsonify({"msg": "Email already exists"}), 400
+    user = User(username=username, email=email, password=hashed_password)
+    db.session.add(user)
+    db.session.commit()
+    return jsonify({"msg": "User created successfully"}), 200
+  
 
-# O ruta de test
-@app.route('/test', methods=['GET', 'POST'])
-def test():
-    if request.method == "GET":
-        return jsonify({"response": "Get request succesfull"})
-    elif request.method == "POST":
-        name = request.json['name']
-        return jsonify({"response": "Hi " + name})
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    if not data:
+        return jsonify({"msg": "Missing JSON in request"}), 400
+    email = data['email']
+    password = data['password']
+    if not email:
+        return jsonify({"msg": "Missing email parameter"}), 400
+    if not password:
+        return jsonify({"msg": "Missing password parameter"}), 400
+    user = User.query.filter_by(email=email).first()
+    if not user:
+        return jsonify({"msg": "Bad username or password"}), 401
+    if not bcrypt.check_password_hash(user.password, password):
+        return jsonify({"msg": "Bad username or password"}), 401
+    access_token = create_access_token(identity=user.username)
+    refresh_token = create_refresh_token(identity=user.username)
+    resp = jsonify({'message': 'Login successful'})
+    set_access_cookies(resp, access_token)
+    resp.set_cookie('refresh_token', refresh_token, httponly=True)
+    return resp, 200
+ 
+  
+@app.route('/logout', methods=['POST'])
+def logout():
+    response = jsonify({'message': 'Logout successful'})
+    unset_jwt_cookies(response)
+    return response, 200
+
+# ===============================
 
 
+# Products CRUD operations
+# ===============================
 @app.route('/products', methods=['GET'])
 def getProducts():
     global Products
@@ -45,7 +97,32 @@ def getProducts():
     return jsonify(output)
 
 
+@app.route('/products/<int:id>/', methods=['PUT'])
+@jwt_required()
+def updateProducts(id):
+    data = request.get_json()
+    currentProduct = Products.query.get(id)
+    currentProduct.product_name = data['product_name']
+    currentProduct.price = data['price']
+    currentProduct.category = data['category']
+    currentProduct.description = data['description']
+    currentProduct.ingredients = data['ingredients']
+    currentProduct.allergens = data['allergens']
+    db.session.commit()
+    return jsonify({'message': 'Product updated!'})
+
+
+@app.route('/products/<int:id>/', methods=['DELETE'])
+@jwt_required()
+def  deleteProduct(id):
+    currentProduct = Products.query.get(id)
+    db.session.delete(currentProduct)
+    db.session.commit()
+    return jsonify({'message': 'Product deleted!'})
+
+
 @app.route('/products', methods=['POST'])
+@jwt_required()
 def postProducts():
     productData = request.get_json()
     product = Products(
@@ -60,8 +137,4 @@ def postProducts():
     db.session.commit()
     return jsonify(productData)
 
-
-@app.route('/')
-def index():
-    return "Hello World"
-
+# ===============================
